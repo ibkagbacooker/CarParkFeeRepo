f2993b8d1171c3724661f49f46a1c566ea78cfd2
ibukunoluwa <ibukunogunfowokan@gmail.com>
2024-02-01 04:08:16 +0100

    final commit

--- obj/Debug/net8.0/c# projects.AssemblyInfo.cs
+++ obj/Debug/net8.0/c# projects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c# projects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef7fdcc1bb07c0fa7878b3e3b2abf50ce042450d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3090af671c3e081eaa14faf696591192ba452fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("c# projects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c# projects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
